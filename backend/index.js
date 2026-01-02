d041e117e052f94364be2360cb9cd0cc961b9f8f
Nikhil Agarwal <agarwalnikhil909@gmail.com>
2026-01-03 00:56:18 +0530

    change commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
 
 const PORT = process.env.PORT || 5000
@@ -6,7 +8,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-import cors from 'cors';
 
 const allowedOrigins = [
   'http://localhost:5173',
